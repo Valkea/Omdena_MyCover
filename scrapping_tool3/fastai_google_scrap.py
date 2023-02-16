ab0c93e7f672096d5093e7354c10f66a6494f4bb
Emmanuel <github-sub@shedge.com>
2023-02-16 15:36:52 +0100

    feature: create a new image scrapper using fastai

---
+++ scrapping_tool3/fastai_google_scrap.py
@@ -0,0 +1,41 @@
+import pathlib
+from fastai.vision.utils import download_images, verify_images
+
+
+def download_images_fastai(
+    input_path: pathlib.Path, output_path: pathlib.Path
+) -> None:
+
+    output_path.mkdir(parents=True, exist_ok=True)
+
+    print("  - Downloading Images -   ")
+    download_images(url_file=input_path, dest=output_path)
+    print("  - Done -   ")
+
+
+def check_images_fastai(
+    output_path: pathlib.Path
+) -> None:
+
+    print("  - Checking Images -   ")
+    # verify_images(output_path, delete=True, img_format=f"{c} %d")
+    li = verify_images(output_path.ls())
+    print(li)
+    print("  - Done -   ")
+
+
+if __name__ == "__main__":
+
+    classes_list = ['roof', 'taillight']
+
+    for c in classes_list:
+        try:
+            input_path = pathlib.Path("inputs", f"{c}_urls.txt")
+            output_path = pathlib.Path("output", f"{c}_damages")
+
+            download_images_fastai(input_path, output_path)
+            check_images_fastai(output_path)
+
+        except Exception as e:
+            print(e)
+
