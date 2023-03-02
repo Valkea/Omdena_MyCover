c1208afb01871027fd64763b6ee4e2b2ac294eef
Emmanuel <github-sub@shedge.com>
2023-03-02 16:38:23 +0100

    refactor: moved models to a 'models' folder
    refactor: black format

--- deployment/API_client_server.py
+++ deployment/API_client_server.py
@@ -3,11 +3,12 @@
 
 import os
 import io
-import pathlib
+from pathlib import Path
 
 from flask import Flask, flash, request, redirect, jsonify, url_for
 from ultralytics import YOLO
 import easyocr
+
 # from PIL import Image
 import cv2
 import numpy as np
@@ -16,20 +17,22 @@ import numpy as np
 
 # --- Load Models ---
 
-model_cdd = YOLO("car_damage_detect.pt")
-model_lpd = YOLO("license_plate_detect_model.pt")
+model_cdd = YOLO(Path("models", "car_damage_detect.pt"))
+model_lpd = YOLO(Path("models", "license_plate_detect_model.pt"))
 
 # --- API Flask app ---
 
 app = Flask(__name__)
 app.secret_key = "super secret key"
-
-
 ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg"}
+
+
 def allowed_file(filename):
     return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
+
 # ########## API ENTRY POINTS (BACKEND) ##########
+
 
 @app.route("/")
 def index():
@@ -41,7 +44,9 @@ def index():
     <h2>Or you can query the entry points to get JSON answers</h2>
     """
 
+
 # ##### PREDICT DAMAGES #####
+
 
 @app.route("/predict_damages/", methods=["GET", "POST"])
 def predict_damages():
@@ -61,7 +66,7 @@ def predict_damages():
             flash("No selected file")
             return redirect(request.url)
 
-        if file and (allowed_file(file.filename) or file.filename == 'file'):
+        if file and (allowed_file(file.filename) or file.filename == "file"):
             print(os.getcwd())
             # filename = secure_filename(file.filename)
 
@@ -79,72 +84,84 @@ def predict_damages():
             predictions_coords = []
 
             for r in results:
-        
+
                 boxes = r.boxes
                 for box in boxes:
-            
-                    coords = box.xyxy[0]  # get box coordinates in (top, left, bottom, right) format
+
+                    coords = box.xyxy[
+                        0
+                    ]  # get box coordinates in (top, left, bottom, right) format
                     classindex = box.cls
 
-                    predictions_classes.append(
-                        model_cdd.names[int(classindex)]
-                    )
-                    predictions_coords.append(
-                        coords.tolist()
-                    )
+                    predictions_classes.append(model_cdd.names[int(classindex)])
+                    predictions_coords.append(coords.tolist())
 
             json_dict = {
-                'classes':predictions_classes,
-                'coords':predictions_coords,
-                'prices':['PRICE-TODO']*len(predictions_classes),
-                'actions': ['REPAIR-REPLACE-TODO']*len(predictions_classes)
+                "classes": predictions_classes,
+                "coords": predictions_coords,
+                "prices": ["PRICE-TODO"] * len(predictions_classes),
+                "actions": ["REPAIR-REPLACE-TODO"] * len(predictions_classes),
             }
 
             args = request.args
-            if args.get('isfrontend'):
-    
-                predictions_merged = '<br>'.join([str(x) for x in zip(json_dict['classes'], json_dict['coords'], json_dict["prices"], json_dict["actions"])])
-                return redirect(url_for('upload_damages', predictions_merged = predictions_merged))
+            if args.get("isfrontend"):
+
+                predictions_merged = "<br>".join(
+                    [
+                        str(x)
+                        for x in zip(
+                            json_dict["classes"],
+                            json_dict["coords"],
+                            json_dict["prices"],
+                            json_dict["actions"],
+                        )
+                    ]
+                )
+                return redirect(
+                    url_for("upload_damages", predictions_merged=predictions_merged)
+                )
 
             else:
                 return jsonify(json_dict)
 
-    return f"This API entrypoint needs a POST requests with a 'file' parameter"
+    return "This API entrypoint needs a POST requests with a 'file' parameter"
 
 
 # ##### PREDICT PLATE NUMBER #####
 
 reader = None
 
-def get_text( image, coords ):
-        """ Obtains the license plate number from the license plate """
 
-        # Initialize easyocr reader if needed        
-        global reader
-        if reader is None:
-            reader = easyocr.Reader(['en'])
+def get_text(image, coords):
+    """ Obtains the license plate number from the license plate """
 
-        # Extract plate coordinates
-        x1,y1,x2,y2 = int(coords[0]), int(coords[1]), int(coords[2]), int(coords[3])
+    # Initialize easyocr reader if needed
+    global reader
+    if reader is None:
+        reader = easyocr.Reader(["en"])
 
-        # Preprocess plate image (crop / gray / ...)
-        nimg = np.array(image)
-        img = cv2.cvtColor(nimg, cv2.COLOR_RGB2BGR)
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        img_precise = img[y1:y2, x1:x2]
-        gray = cv2.cvtColor(img_precise, cv2.COLOR_RGB2GRAY)
-        # cv2.imwrite("plate.png", gray)
+    # Extract plate coordinates
+    x1, y1, x2, y2 = int(coords[0]), int(coords[1]), int(coords[2]), int(coords[3])
 
-        # Try to read text from image
-        result = reader.readtext(gray)
+    # Preprocess plate image (crop / gray / ...)
+    nimg = np.array(image)
+    img = cv2.cvtColor(nimg, cv2.COLOR_RGB2BGR)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img_precise = img[y1:y2, x1:x2]
+    gray = cv2.cvtColor(img_precise, cv2.COLOR_RGB2GRAY)
+    # cv2.imwrite("plate.png", gray)
 
-        # Parse results        
-        text = "NOT READABLE"
-        for res in result:
-            if len(res[1]) in [8,9] and res[2] > 0.1:
-                text = res[1]
+    # Try to read text from image
+    result = reader.readtext(gray)
 
-        return text
+    # Parse results
+    text = "NOT READABLE"
+    for res in result:
+        if len(res[1]) in [8, 9] and res[2] > 0.1:
+            text = res[1]
+
+    return text
+
 
 @app.route("/predict_plate/", methods=["GET", "POST"])
 def predict_plate():
@@ -164,10 +181,10 @@ def predict_plate():
             flash("No selected file")
             return redirect(request.url)
 
-        if file and (allowed_file(file.filename) or file.filename == 'file'):
+        if file and (allowed_file(file.filename) or file.filename == "file"):
             print(os.getcwd())
             # filename = secure_filename(file.filename)
-            
+
             # Open POST file with PIL
             # image_bytes = Image.open(io.BytesIO(file.read()))
 
@@ -182,49 +199,53 @@ def predict_plate():
             predictions_texts = []
 
             for r in results:
-        
+
                 boxes = r.boxes
                 for box in boxes:
-            
-                    coords = box.xyxy[0]  # get box coordinates in (top, left, bottom, right) format
+
+                    coords = box.xyxy[
+                        0
+                    ]  # get box coordinates in (top, left, bottom, right) format
                     text = get_text(image_bytes, coords)
 
-                    predictions_coords.append(
-                        coords.tolist()
-                    )
-                    predictions_texts.append(
-                        text
-                    )
+                    predictions_coords.append(coords.tolist())
+                    predictions_texts.append(text)
 
             json_dict = {
-                'texts':predictions_texts,
-                'coords':predictions_coords,
+                "texts": predictions_texts,
+                "coords": predictions_coords,
             }
 
             args = request.args
-            if args.get('isfrontend'):
-                predictions_merged = '<br>'.join([str(x) for x in zip(json_dict['coords'], json_dict['texts'])])
-                return redirect(url_for('upload_plate', predictions_merged = predictions_merged))
+            if args.get("isfrontend"):
+                predictions_merged = "<br>".join(
+                    [str(x) for x in zip(json_dict["coords"], json_dict["texts"])]
+                )
+                return redirect(
+                    url_for("upload_plate", predictions_merged=predictions_merged)
+                )
 
             else:
                 return jsonify(json_dict)
 
-    return f"This API entrypoint needs a POST requests with a 'file' parameter"
-
+    return "This API entrypoint needs a POST requests with a 'file' parameter"
 
 
 # ########## DEMO FRONTEND ##########
 # This could be a different Flask script totally independant from the API!
 
+
 def print_upload_form(API_URL, target, predictions_merged=None):
-    """ Define the HTML form used to send images / videos 
-        to the API 'predict' endpoint
+    """Define the HTML form used to send images / videos
+    to the API 'predict' endpoint
     """
 
     if predictions_merged is not None:
-        predictions_merged_display = f"<h1>Predicted classes</h1><p>{predictions_merged}</p>"
+        predictions_merged_display = (
+            f"<h1>Predicted classes</h1><p>{predictions_merged}</p>"
+        )
     else:
-        predictions_merged_display = ''
+        predictions_merged_display = ""
 
     return f"""
     <!doctype html>
@@ -243,25 +264,28 @@ def print_upload_form(API_URL, target, predictions_merged=None):
     </html>
     """
 
+
 @app.route("/upload_damages/")
 def upload_damages():
 
     API_URL = request.url_root
     print("API_URL:", API_URL)
-        
+
     args = request.args
 
-    return print_upload_form(API_URL, 'predict_damages', args.get('predictions_merged'))
+    return print_upload_form(API_URL, "predict_damages", args.get("predictions_merged"))
+
 
 @app.route("/upload_plate/")
 def upload_plate():
 
     API_URL = request.url_root
     print("API_URL:", API_URL)
-        
+
     args = request.args
 
-    return print_upload_form(API_URL, 'predict_plate', args.get('predictions_merged'))
+    return print_upload_form(API_URL, "predict_plate", args.get("predictions_merged"))
+
 
 # ########## START BOTH API & FRONTEND ##########
 
